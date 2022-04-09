c398de5f5308a50750aaaf70323f1883a5c12312
grisha0404 <grisha.kom@gmail.com>
2022-04-09 22:47:18 +0300

    portfolio

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import style from './main.module.css'
+import styleC from '../../command/styles/container.module.css'
+
+export const Main = () => {
+    return (
+        <div className={style.main}>
+            <div className={styleC.container}>
+                <div className={style.text}>
+                    <span>Welcome to my world</span>
+                    <h1>Hello I'm
+                        <span> Grigoriy Kom </span>
+                    </h1>
+                    <span>a Front-end Developer</span>
+                </div>
+                <div className={style.photo}>
+
+                </div>
+            </div>
+        </div>
+
+    );
+};
