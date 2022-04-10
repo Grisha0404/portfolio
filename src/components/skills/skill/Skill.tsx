225071aefef77ed0777cba4a258a7f7c7dc43898
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-10 14:12:41 +0300

    all dune portfolio

---
+++ src/components/skills/skill/Skill.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import style from "./skill.module.css";
+
+type SkillType = {
+    title: string,
+    description: string
+}
+
+export const Skill = (props: SkillType) => {
+
+    return (
+        <div>
+            <div className={style.skill}>
+                <h3>{props.title}</h3>
+                <p>
+                    {props.description}
+                </p>
+            </div>
+            <div className={style.skillProgress}>
+                <div className={style.skillProgressBlock} style={{width:props.description}}>
+                </div>
+            </div>
+        </div>
+    );
+};
