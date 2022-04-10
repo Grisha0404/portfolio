720fd660faf1565ce5a6c25249ae7ae5a6804908
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-10 14:12:05 +0300

    all dune portfolio

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -9,11 +9,12 @@ export const Features = () => {
             <div className={`${styleC.container} ${style.featuresBlockContainer} `}>
                 <h2>What I do</h2>
                 <div className={style.block}>
-                    <Feature title={"Business Stratagy"}
+                    <Feature title={"Business Strategy"}
                              descriptions={'I throw myself down among the tall grass by the stream as I\n' +
                                  '                lie close to the earth.'}/>
                     <Feature title={"App Development"}
-                             descriptions={'I throw myself down among the tall grass by the stream as I lie close to the earth.'}/>
+                             descriptions={' It uses a dictionary of over 200 Latin words, combined with\n' +
+                                 '                                        a handful of model sentence.'}/>
                     <Feature title={"App Development"}
                              descriptions={'It uses a dictionary of over 200 Latin words, combined with\n' +
                                  'a handful of model sentence.'}/>
