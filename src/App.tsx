b87ddc614a07cd5aa725b8e3efc0889a59676ba9
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-10 14:11:31 +0300

    full portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import './App.css';
 import {Header} from "./components/header/Header";
 import {Main} from "./components/main/Main";
 import {Features} from "./components/features/Features";
+import {Skills} from "./components/skills/Skills";
+import {Projects} from "./components/projects/Porojects";
+import {Contacts} from "./components/contacts/Contacts";
 
 function App() {
     return (
         <div className='App'>
             <Header/>
             <Main/>
+            <Skills/>
             <Features/>
+            <Projects/>
+            <Contacts/>
         </div>
     );
 }
