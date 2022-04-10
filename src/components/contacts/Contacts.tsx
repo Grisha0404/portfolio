5cb4ce8ee9ba74ba90a091a6d32eb63017cebcdc
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-10 14:11:52 +0300

    all dune portfolio

---
+++ src/components/contacts/Contacts.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import style from "./contacts.module.css";
+import {Contact} from "./contact/Contact";
+import styleC from "../../command/styles/container.module.css";
+
+export const Contacts = () => {
+    return (
+        <div className={style.contacts}>
+            <div className={`${styleC.container} ${style.contactBlockContainer} `}>
+                <div className={style.messageBlock}>
+            <h2>
+                Get in touch
+            </h2>
+                    <input/>
+                    <input/>
+                    <input/>
+                </div>
+                <div className={style.contactsBlock}>
+                    <h2>Contact info</h2>
+                    <div className={style.infoBlock}>
+                    <Contact title={"Phone:"} descriptions={'+375336133523'}/>
+                    <Contact title={"Email:"} descriptions={'grisha.kom@gmail.com'}/>
+                    <Contact title={"Location:"} descriptions={"Minsk, Nikoly Tesly street, 7"}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
