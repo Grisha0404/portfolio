75ed5104a99c8acbac079ecaad19b8265f4977e9
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-11 16:36:15 +0300

    contacts footer mod

--- src/components/projects/project/project.tsx
+++ src/components/projects/project/project.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import style from "./project.module.css";
+import {NavLink} from "react-router-dom";
 
 type ProjectType = {
-    title: string
+    title: string,
+    link:string
 }
 
 export const Project = (props: ProjectType) => {
     return (
         <div>
             <div className={style.icon}>
+                <NavLink to={props.link}>{props.title}</NavLink>
             </div>
             <div style={{textAlign:"center"}}>{props.title}</div>
         </div>
