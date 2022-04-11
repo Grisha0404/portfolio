2630e5284bf32bb33645294f1adb457864617fd4
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-11 16:36:09 +0300

    contacts footer mod

--- src/components/projects/Porojects.tsx
+++ src/components/projects/Porojects.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from "./projects.module.css";
 import styleC from "../../command/styles/container.module.css";
 import {Project} from "./project/project";
+import {HashRouter} from "react-router-dom";
 
 export const Projects = () => {
     return (
@@ -9,9 +10,11 @@ export const Projects = () => {
             <div className={`${styleC.container} ${style.projectsBlockContainer} `}>
                 <h2>My Projects</h2>
                 <div className={style.block}>
-                    <Project title={"Social"}/>
-                    <Project title={"Counter"}/>
-                    <Project title={"TodoList"}/>
+                    <HashRouter>
+                    <Project title={"Social"} link={'/social'}/>
+                    <Project title={"Counter"} link={'/counter'}/>
+                    <Project title={"TodoList"} link={'/todolist'}/>
+                    </HashRouter>
                 </div>
             </div>
         </div>
