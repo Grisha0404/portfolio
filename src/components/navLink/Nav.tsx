6980ee986603da708125bb18291c28f1deed6541
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-11 16:49:18 +0300

    contacts footer mod

--- src/components/navLink/Nav.tsx
+++ src/components/navLink/Nav.tsx
@@ -10,6 +10,7 @@ export const Nav = () => {
             <NavLink to='/home' >Home</NavLink>
             <NavLink to='/skills' >Skills</NavLink>
             <NavLink to='/features' >Features</NavLink>
+            <NavLink to='/projects' >Projects</NavLink>
             <NavLink to='/contact' >Contact</NavLink>
         </div>
     );
