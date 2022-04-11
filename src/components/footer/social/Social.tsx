75c8d6880c3b09a99b446baba0879cf97e829e0a
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-11 16:36:22 +0300

    contacts footer mod

---
+++ src/components/footer/social/Social.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from "./social.module.css";
+
+export const Social = () => {
+    return (
+        <div className={style.social}>
+            <div className={style.icon}></div>
+        </div>
+    );
+};
