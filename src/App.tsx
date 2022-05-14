712d0ac4c403c66e2d20f9309c32c5779ce8bfc7
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-14 22:56:55 +0300

    refactor styles

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ function App() {
         <div className='App'>
             <Header/>
             <Main/>
-            <Skills/>
-            <Features/>
             <Projects/>
+            <Features/>
+            <Skills/>
             <Contacts/>
             <Footer/>
         </div>
