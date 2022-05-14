ed2a673a98bd51791df4b3202cfbb09d4a9ca878
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-14 22:57:36 +0300

    refactor styles

--- src/components/projects/Porojects.tsx
+++ src/components/projects/Porojects.tsx
@@ -3,17 +3,34 @@ import style from "./projects.module.css";
 import styleC from "../../command/styles/container.module.css";
 import {Project} from "./project/project";
 import {HashRouter} from "react-router-dom";
+import {Title} from "../../command/Title/Title";
+import imgTodo from '../../assets/image/3536667_935xp.jpeg'
+import imgSocial from '../../assets/image/maxresdefault.jpeg'
+import imgWeather from '../../assets/image/screenshot.png'
 
 export const Projects = () => {
+    const socialImg = {
+        backgroundImage: `url(' ${imgSocial}')`
+    }
+    const todolist = {
+        backgroundImage: `url('${imgTodo}')`
+    }
+    const weatherApp = {
+        backgroundImage: `url('${imgWeather}')`
+    }
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleC.container} ${style.projectsBlockContainer} `}>
-                <h2>My Projects</h2>
+                <Title title={'My Projects'}/>
                 <div className={style.block}>
                     <HashRouter>
-                    <Project title={"Social"} link={'/social'}/>
-                    <Project title={"Counter"} link={'/counter'}/>
-                    <Project title={"TodoList"} link={'/todolist'}/>
+                        <Project title={"Social"} link={'/social'}
+                                icon={socialImg}
+                                 infoTitle={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
+                        <Project title={"TodoList"} link={'/counter'} icon={todolist}
+                                 infoTitle={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
+                        <Project title={"Weather App"} link={'/todolist'} icon={weatherApp}
+                                 infoTitle={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
                     </HashRouter>
                 </div>
             </div>
