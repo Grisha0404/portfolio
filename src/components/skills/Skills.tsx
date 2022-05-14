094accf5e1db794de76eda3945f32fe64b2f3eeb
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-14 22:57:22 +0300

    refactor styles

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import style from "./skills.module.css"
 import styleC from "../../command/styles/container.module.css";
 import {Skill} from "./skill/Skill";
+import {Title} from "../../command/Title/Title";
 
 
 export const Skills = () => {
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleC.container} ${style.skillsBlockContainer} `}>
-                <span>
-                    <p>My level of knowledge in some tools</p>
-                    <h2>My Skills</h2>
-                </span>
+                <Title title={'My Skills'}/>
                 <div className={style.blockSkills}>
                 <Skill title={'HTML/CSS'} description={'80%'}/>
                 <Skill title={'JavaScript'} description={'45%'}/>
