c95131d36720c6a43131e50d920ef064c122355a
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-15 19:00:24 +0300

    SASS

--- src/components/navLink/Nav.tsx
+++ src/components/navLink/Nav.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-import style from './nav.module.css'
-
-
+import style from './nav.module.scss'
 
 export const Nav = () => {
     return (
         <div className={style.navLink}>
-            <NavLink to='/home' >Home</NavLink>
-            <NavLink to='/skills' >Skills</NavLink>
-            <NavLink to='/features' >Features</NavLink>
-            <NavLink to='/projects' >Projects</NavLink>
-            <NavLink to='/contact' >Contact</NavLink>
+            <NavLink to='/home'  className={style.link}>Home</NavLink>
+            <NavLink to='/projects' className={style.link}>Projects</NavLink>
+            <NavLink to='/features' className={style.link}>Features</NavLink>
+            <NavLink to='/skills' className={style.link}>Skills</NavLink>
+            <NavLink to='/contact' className={style.link}>Contact</NavLink>
         </div>
     );
 };
