0a41b13b57a739185a183b0fe3e543154706cc04
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-15 19:00:00 +0300

    SASS

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
-import style from './footer.module.css'
+import style from './footer.module.scss'
 import styleC from "../../command/styles/container.module.css";
 import {Social} from "./social/Social";
+import imgLin from "../../assets/image/icons8-linkedin-circled-100.png";
+import instImg from "../../assets/image/icons8-instagram-100.png";
+import gitImg from "../../assets/image/icons8-github-100.png";
 
 export const Footer = () => {
+    const link = {
+        backgroundImage: `url(${imgLin})`
+    }
+    const inst = {
+        backgroundImage: `url(${instImg})`
+    }
+    const git = {
+        backgroundImage: `url(${gitImg})`
+    }
     return (
         <div className={style.footerBlock}>
             <div className={`${styleC.container} ${style.footerBlockContainer} `}>
-                <h2>Grigoriy Kom</h2>
+                <h2>Gregory Kom</h2>
                 <div className={style.socialBlock}>
-                    <Social/>
-                    <Social/>
-                    <Social/>
+                    <Social icon={link}/>
+                    <Social icon={inst}/>
+                    <Social icon={git}/>
                 </div>
-                <span>© 2022. All rights reserved</span>
+                <p>© 2022. All rights reserved</p>
             </div>
 
         </div>
