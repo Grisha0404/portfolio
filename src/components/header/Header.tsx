a1021ed53710c0e2f3d3f7384da01a0289f1ea69
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-23 13:28:07 +0300

    router dom

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import style from './header.module.scss'
 import {Nav} from "../navLink/Nav";
-import {HashRouter} from "react-router-dom";
 
 export const Header = () => {
     return (
         <div className={style.head}>
-            <HashRouter>
             <Nav/>
-            </HashRouter>
         </div>
     );
 };
