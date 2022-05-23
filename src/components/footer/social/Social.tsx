a95b1e2f5c3e5fabdc61d20b1ebf63d95c32390e
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-23 13:28:24 +0300

    router dom

--- src/components/footer/social/Social.tsx
+++ src/components/footer/social/Social.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import style from "./social.module.css";
+import {Link, useNavigate} from "react-router-dom";
 
-type SocialType={
-    icon:{
+type SocialType = {
+    icon: {
         backgroundImage: string;
-    }
+    },
+    link: string,
 }
 
-export const Social = (props:SocialType) => {
+export const Social = (props: SocialType) => {
+    const navigate = useNavigate()
+    const clickHandler = () => {
+        navigate(props.link)
+    }
     return (
         <div className={style.social}>
-            <div className={style.icon} style={props.icon}></div>
+                <div className={style.icon} style={props.icon} onClick={clickHandler}></div>
         </div>
     );
 };
